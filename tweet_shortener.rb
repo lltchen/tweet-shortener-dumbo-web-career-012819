e0c28b60525248a22c3f70f833419b60e6e5b774
lltchen <lltchen1@gmail.com>
2019-01-23 04:33:04 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,11 +1,11 @@
 def dictionary
 
-  { 
+  {
     "hello" => "hi",
-    "to" => "2", 
-    "two" => "2", 
-    "too" => "2", 
-    "for" => "4", 
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
     "four" => "4",
     "be" => "b",
     "you" => "u",
@@ -18,13 +18,12 @@ end
 def word_substituter(tweet_one, dictionary)
   tweet = tweet_one.split
     tweet.collect do |words|
-      dictionary.collect  do |keys,value|                                                                                                                                                                       
+      dictionary.collect  do |keys,value|
           if keys.include?(words)
             words.gsub(words,value)
           else
             return "not working"
         end
-      end    
+      end
     end
 end
-  
