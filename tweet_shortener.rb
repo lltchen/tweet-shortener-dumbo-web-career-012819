d490b49cf0aa95610c381992420cf88e02f415dd
lltchen <lltchen1@gmail.com>
2019-01-23 04:38:16 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -16,6 +16,22 @@ def dictionary
 end
 
 def word_substituter(tweet_one)
+  def dictionary
+
+    {
+      "hello" => "hi",
+      "to" => "2",
+      "two" => "2",
+      "too" => "2",
+      "for" => "4",
+      "four" => "4",
+      "be" => "b",
+      "you" => "u",
+      "at" => "@",
+      "and" => "&"
+    }
+
+  end
   tweet = tweet_one.split
     tweet.collect do |words|
       dictionary.collect  do |keys,value|
